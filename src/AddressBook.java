6fc85b1ac1a087206c56ba417d12d5c3f771c281
Zhangjason308 <zhangjason308@gmail.com>
2022-09-19 02:23:58 -0400

    Added a method to AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,15 +4,15 @@ public class AddressBook {
     private ArrayList<BuddyInfo> addresses;
 
     public AddressBook() {
-        this.addresses = new ArrayList<BuddyInfo>();
+        this.addresses = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo address) {
-        this.addresses.add(address);
+        addresses.add(address);
     }
 
     public void removeBuddy(BuddyInfo address) {
-        this.addresses.remove(address);
+        addresses.remove(address);
     }
     public static void main(String[] args) {
     System.out.println("Address Book");
