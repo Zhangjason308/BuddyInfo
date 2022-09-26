73562f8d0ffc2510f7132f943014c452205dcbda
Zhangjason308 <zhangjason308@gmail.com>
2022-09-26 03:49:13 -0400

    Added new method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -18,6 +18,11 @@ public class AddressBook {
         }
         return null;
     }
+
+    public void randomFunction(int index) {
+        System.out.println("Part 3 question 4");
+    }
+
     public static void main(String[] args) {
     System.out.println("Address Book");
     BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
@@ -26,6 +31,7 @@ public class AddressBook {
     addressBook.removeBuddy(0); //value changed from 0 to 1
         //This is for Github question 11 -> need to push in order ot show changes in github, elsewise it commits only affect local repo
         //This is for question 17, editing the github file
+        //Question 4 of Part 3, edit the file on branch
 
     }
 }
